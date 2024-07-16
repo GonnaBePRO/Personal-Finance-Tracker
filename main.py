eb33d871d8fa87fb8c2699d14801f1773990d7b3
Michu <michu9139@gmail.com>
2024-07-16 13:08:32 +0200

    New Dictionary & CSV Writer

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 class CSV:
     CSV_FILE = "finance_data.csv" #class variable
+    COLUMNS = ["date", "amount", "category", "description"]
     
     #This will have access to the classess itself but it won t have access to its instance
     @classmethod
@@ -12,8 +13,22 @@ class CSV:
         try:
             pd.read_csv(cls.CSV_FILE)
         except FileNotFoundError:
-            df = pd.DataFrame(columns=["date", "amount", "category", "description"])
+            df = pd.DataFrame(columns= cls.COLUMNS)
             df.to_csv(cls.CSV_FILE, index=False)
 
+    @classmethod
+    def add_entry(cls, date, amount, category, description):    #adding entries using dictionary
+        new_entry = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description": description
+        }
+        with open(cls.CSV_FILE, "a", newline="") as csvfile:    #a = pending to the end of the file
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS) #csv writer
+            writer.writerow(new_entry)
+        print("Entry added successfully")
+
 CSV.initialize_csv()
+CSV.add_entry("16-07-2024", 435.28, "Income", "Salary")
         
